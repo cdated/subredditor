a4cb47b928f6cd7f62780eb620e66a9494b17302
Brandon T. Fields <brandon@cdated.com>
2015-11-30 20:46:03 -0500

    Generate a full graph and a graph without age restricted content

--- generate_graph.py
+++ generate_graph.py
@@ -24,7 +24,52 @@ def main():
 
             related_subs[title] = links
 
+    generate_full_graph(related_subs, subscribers, adult, min_subscribers=0)
+    generate_censored_graph(related_subs, subscribers, adult, min_subscribers=100)
     generate_adult_graph(related_subs, subscribers, adult, min_subscribers=100)
+
+def generate_full_graph(related_subs, subscribers, adult, min_subscribers):
+
+    g = Digraph('G', filename='full.gv')
+
+    edges_added = 0
+    for key in related_subs:
+        for sub in related_subs[key]:
+            if not sub:
+                continue
+
+            # Filter: only include edge if sub has # subscribers
+            if sub in subscribers:
+                subscriber_cnt = subscribers[sub]
+                if subscriber_cnt >= min_subscribers:
+                    log_cnt = math.log2(subscriber_cnt)
+                    g.edge(key, sub, weight=str(log_cnt))
+                    print("Edge count: " + str(edges_added))
+                    edges_added += 1
+
+    g.save()
+
+def generate_censored_graph(related_subs, subscribers, adult, min_subscribers):
+
+    g = Digraph('G', filename='censored.gv')
+
+    edges_added = 0
+    for key in related_subs:
+        for sub in related_subs[key]:
+            if not sub:
+                continue
+
+            # Filter: only include edge if sub has # subscribers
+            if sub in subscribers and not sub in adult:
+                subscriber_cnt = subscribers[sub]
+                if subscriber_cnt >= min_subscribers:
+                    log_cnt = math.log2(subscriber_cnt)
+                    g.edge(key, sub, weight=str(log_cnt))
+                    print("Edge count: " + str(edges_added))
+                    edges_added += 1
+
+    g.save()
+
 
 def generate_adult_graph(related_subs, subscribers, adult, min_subscribers):
 
