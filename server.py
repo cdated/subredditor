33058680e92824e493d6c51e5bf9ee98f86b2494
Brandon T. Fields <brandon@cdated.com>
2016-01-05 23:46:40 -0500

    Only generate new graph data if it doesn't exist

--- server.py
+++ server.py
@@ -15,6 +15,10 @@ def my_link():
     breadth = int(request.args.get('breadth'))
     depth = int(request.args.get('depth'))
 
+    nsfw_str = ''
+    if nsfw:
+        nsfw_str = '_nsfw'
+
     if breadth > 6:
         breadth = 6
 
@@ -27,13 +31,19 @@ def my_link():
     # Graph parameters
     rec.output_path = 'static'
 
-    (result, msg) = rec.generate_graph(seed, False)
-
-    if result == 'Sucess':
-        filename = msg
+    filename = rec.output_path + '/' + seed + '_b' + str(breadth) + '_d' + str(depth) + nsfw_str + '.json'
+    if os.path.exists(filename):
+        # graph data exist, skip to render
         html = render_template('graph.html', filename=filename)
     else:
-        html = msg
+        # generate graph data
+        (result, msg) = rec.generate_graph(seed, False)
+
+        if result == 'Sucess':
+            filename = msg
+            html = render_template('graph.html', filename=filename)
+        else:
+            html = msg
 
     return html
 
